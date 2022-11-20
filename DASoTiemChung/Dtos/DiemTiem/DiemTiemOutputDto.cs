c2552bb1a82025155eece1f533e76bf9ca100c84
Cao Huy <huy.cao.98031@gmail.com>
2022-11-20 07:32:34 +0700

    create diem_tiem complete

---
+++ DASoTiemChung/Dtos/DiemTiem/DiemTiemOutputDto.cs
@@ -0,0 +1,23 @@
+﻿using DASoTiemChung.Models;
+using System.Collections.Generic;
+
+namespace DASoTiemChung.Dtos.DiemTiem
+{
+    public class DiemTiemOutputDto
+    {
+        public int MaDiemTiem { get; set; }
+        public int? MaXaPhuong { get; set; }
+        public int? MaQuanHuyen { get; set; }
+        public int? MaTinhThanhPho { get; set; }
+        public string TenDiemTiem { get; set; }
+        public string SoNha { get; set; }
+        public XaPhuong XaPhuongNavigation { get; set; }
+        public QuanHuyen QuanHuyenNavigation { get; set; }
+        public TinhThanhPho TinhThanhPhoNavigation { get; set; }
+        public string NguoiDungDau { get; set; }
+        public string NguoiTiem { get; set; }
+        public virtual ICollection<PhieuNhap> PhieuNhaps { get; set; }
+        public virtual ICollection<PhieuXuat> PhieuXuats { get; set; }
+        public virtual ICollection<VacXinTheoLo> VacXinTheoLos { get; set; }
+    }
+}
---
+++ DASoTiemChung/Dtos/DiemTiem/SearchDiemTiemDto.cs
@@ -0,0 +1,16 @@
+﻿using DASoTiemChung.Filter;
+
+namespace DASoTiemChung.Dtos.DiemTiem
+{
+    public class SearchDiemTiemDto : PagedRequestDto
+    {
+
+        public string TenXaPhuong { get; set; }
+        public string TenQuanHuyen { get; set; }
+        public string TenTinhThanh { get; set; }
+        public string TenDiemTiem { get; set; }
+        public string SoNha { get; set; }
+        public string NguoiDungDau { get; set; }
+        public string NguoiTiem { get; set; }
+    }
+}
